0e9ad85f2f0fae1fe40c2bb16e2e421d21f65093
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-08-07 08:09:11 +0530

    Seed sample data for "ability_user" table.

---
+++ app/database/seeds/AbilityUserSeeder.php
@@ -0,0 +1,90 @@
+<?php
+
+class AbilityUserSeeder extends Seeder
+{
+
+	public function run ()
+	{
+		$abilityUsers = [
+			[
+				'ability_id' => 1 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 2 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 3 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 4 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 5 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 6 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 7 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 8 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 9 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 10 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 11 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 12 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 13 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 14 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 15 ,
+				'user_id'	 => 1
+			] ,
+			[
+				'ability_id' => 16 ,
+				'user_id'	 => 1
+			] ,
+//			[
+//				'ability_id' =>  ,
+//				'user_id'	 => 
+//			] ,
+		] ;
+
+		foreach ( $abilityUsers as $abilityUser )
+		{
+			$abilityId	 = $abilityUser[ 'ability_id' ] ;
+			$userId		 = $abilityUser[ 'user_id' ] ;
+
+			$user = User::find ( $userId ) ;
+
+			$user -> abilities () -> attach ( $abilityId ) ;
+		}
+	}
+
+}
