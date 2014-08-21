6635d703b988db6a9551ab9598e8f09301cf7a6d
Asanka Jagoda <asanka@thesanmark.com>
2014-08-21 17:50:26 +0530

    Add Purchase UI finished.

--- app/views/web/processes/purchases/add.blade.php
+++ app/views/web/processes/purchases/add.blade.php
@@ -2,65 +2,114 @@
 
 @section('body')
 
-<h2>Add Purchase</h2>
-@if($errors->count()>0)
-<ul>
-	@foreach($errors->all() as $error)
-	<li>{{$error}}</li>
-	@endforeach
-</ul>
-@endif
-{{Form::open()}}
-<table>
-	<tr>
-		<td>Date</td>
-		<td>{{Form::input ( 'date','purchase_date',null)}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tr>
-		<td>Vendor</td>
-		<td>{{Form::select('vendor_id',\Models\Vendor::getVendorsForHtmlSelect(),null)}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tr>
-		<td>Print Invoice Number</td>
-		<td>{{Form::text('printed_invoice_num',null)}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tr>
-		<td>Completely Paid</td>
-		<td>{{Form::checkbox('is_paid')}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tr>
-		<td>Other Expense Amount</td>
-		<td>{{Form::text('other_expense_amount')}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tr>
-		<td>Other Expense Total</td>
-		<td>{{Form::text('other_expense_total')}}</td>
-		<td colspan="4"></td>
-	</tr>
-	<tbody id="add_purchase_items">
-		<?php $rowCounter = 0 ; ?>
-		@foreach($itemRows as $itemRow)
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Add Purchase</h3>
+	</div>
+	<div class="panel-body">
 
-		<tr>
-			<td>{{Form::hidden('item_id_'.$itemRow->id,$itemRow->id)}}{{$itemRow->name}}</td>
-			<td>{{Form::input('number','buying_price_'.$itemRow->id,$itemRow->current_buying_price,['step'=>'any'])}}</td>
-			<td>{{Form::input('number','quantity_'.$itemRow->id,null,['placeholder'=>'Quantity','step'=>'any'])}}</td>
-			<td>{{Form::input('number','free_quantity_'.$itemRow->id,null,['placeholder'=>'Free Quantity','step'=>'any'])}}</td>
-			<td>{{Form::input('date','exp_date_'.$itemRow->id,null)}}</td>
-			<td>{{Form::text('batch_number_'.$itemRow->id,null,['placeholder'=>'Batch Number'])}}</td>
-		</tr>
-		<?php //$rowCounter++; ?>
-		@endforeach
+		@if($errors->count()>0)
+		<ul class="errorstring">
+			@foreach($errors->all() as $error)
+			<li>{{$error}}</li>
+			@endforeach
+		</ul>
+		@endif
 
-	</tbody>
-	<tr>
-		<td colspan="6" style="text-align:right">{{Form::hidden('row_counter',$rowCounter)}}{{Form::submit('Submit')}}</td>
-	</tr>
-</table>
-{{Form::close()}}
+		{{Form::open(['class'=>'form-horizontal', 'role'=>'form'])}}
+		<br />
+		<div class="form-group">
+			{{Form::label('date', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::input ( 'date','purchase_date', null, array('class' => 'form-control'))}}</td>
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('vendor', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('vendor_id',\Models\Vendor::getVendorsForHtmlSelect(), null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label(null, 'Print Invoice Number', array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('printed_invoice_num',null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label(null, 'Other Expense Amount', array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('other_expense_amount',null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label(null, 'Other Expense Total', array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('other_expense_total',null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label(null, 'Completely Paid', array('class' => 'col-sm-2 control-label', 'style'=>'padding-top: 0;'))}}
+			<div class="col-sm-3">
+				{{Form::checkbox('is_paid')}}
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<div class="row">
+					<div class="col-sm-2"><b>Price</b></div>
+					<div class="col-sm-2"><b>Quantity</b></div>
+					<div class="col-sm-2"><b>Free Quantity</b></div>
+					<div class="col-sm-3"><b>Date</b></div>
+					<div class="col-sm-3"><b>Batch Number</b></div>
+				</div>			
+			</div>			
+		</div>
+
+		<div id="add_purchase_items">
+			<?php $rowCounter = 0 ; ?>
+			@foreach($itemRows as $itemRow)
+
+			<div class="form-group">
+				{{Form::hidden('item_id_'.$itemRow->id,$itemRow->id)}}
+				{{Form::label(null, $itemRow->name, array('class' => 'col-sm-2 control-label'))}}
+				<div class="col-sm-10">
+					<div class="row">
+						<div class="col-sm-2">
+							{{Form::input('number','buying_price_'.$itemRow->id,$itemRow->current_buying_price, array('class' => 'form-control', 'step'=>'any'))}}
+						</div>
+						<div class="col-sm-2">
+							{{Form::input('number','quantity_'.$itemRow->id, null, array('class' => 'form-control', 'step'=>'any'))}}
+						</div>
+						<div class="col-sm-2">
+							{{Form::input('number','free_quantity_'.$itemRow->id, null, array('class' => 'form-control', 'step'=>'any'))}}
+						</div>
+						<div class="col-sm-3">
+							{{Form::input('date','exp_date_'.$itemRow->id, null, array('class' => 'form-control'))}}
+						</div>
+						<div class="col-sm-3">
+							{{Form::text('batch_number_'.$itemRow->id, null, array('class' => 'form-control'))}} 
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<?php //$rowCounter++; ?>
+			@endforeach
+
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				{{Form::hidden('row_counter',$rowCounter)}}
+				{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+			</div>
+		</div>
+
+		{{Form::close()}}
+
+	</div>
+</div>
+
 @stop
