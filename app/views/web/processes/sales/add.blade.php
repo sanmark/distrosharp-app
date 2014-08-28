c0a2b68431f8f2dd1855e133600f371d2d44ac9e
Asanka Jagoda <asanka@thesanmark.com>
2014-08-28 17:38:16 +0530

    Create add sales UI.

--- app/views/web/processes/sales/add.blade.php
+++ app/views/web/processes/sales/add.blade.php
@@ -1,60 +1,124 @@
 @extends('web._templates.template')
 
 @section('body')
-<h2>Add Sale</h2>
-{{Form::open()}}
-<table>
-	<tr>
-		<td>{{Form::label('date_time')}}</td>
-		<td>{{Form::input('datetime-local','date_time')}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('customer_id')}}</td>
-		<td>{{Form::select('customer_id',$customers)}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('printed_invoice_number')}}</td>
-		<td>{{Form::text('printed_invoice_number')}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('discount')}}</td>
-		<td>{{Form::input('number','discount', NULL, ['step'=>0.01])}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('is_completely_paid')}}</td>
-		<td>{{Form::checkbox('is_completely_paid')}}</td>
-	</tr>
-</table>
 
-<table border="1">
-	<tr>
-		<th>Item</th>
-		<th>Available Amount</th>
-		<th>Price</th>
-		<th>Paid Q</th>
-		<th>Free Q</th>
-		<th>GR Price</th>
-		<th>GR Q</th>
-		<th>CR Price</th>
-		<th>CR Q</th>
-	</tr>
-	@foreach($items as $item)
-	<tr>
-		<td>{{$item->name}}</td>
-		<td>
-			{{$stockDetails[$item->id]['good_quantity']}}
-			{{Form::hidden('items['.$item->id.'][available_quantity]', $stockDetails[$item->id]['good_quantity'])}}
-		</td>
-		<td>{{Form::input('number','items['.$item->id.'][price]',$item->current_selling_price,['step'=>0.01])}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][paid_quantity]',NULL)}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][free_quantity]',NULL)}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][good_return_price]',NULL,['step'=>0.01])}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][good_return_quantity]',NULL)}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][company_return_price]',NULL,['step'=>0.01])}}</td>
-		<td>{{Form::input('number','items['.$item->id.'][company_return_quantity]',NULL)}}</td>
-	</tr>
-	@endforeach
-</table>
-{{Form::submit('Submit')}}
-{{Form::close()}}
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Add Sale</h3>
+	</div>
+	<div class="panel-body">
+
+		{{Form::open(['class'=>'form-horizontal', 'role'=>'form'])}}
+		<br />
+		<div class="form-group">
+			{{Form::label('date_time', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::input('datetime-local','date_time', null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('customer_id', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('customer_id',$customers, null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('printed_invoice_number', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('printed_invoice_number', null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('discount', null, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::input('number','discount', null, array('class' => 'form-control'), ['step'=>0.01])}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('is_completely_paid', null, array('class' => 'col-sm-2 control-label', 'style'=>'padding-top: 0;'))}}
+			<div class="col-sm-3">
+				{{Form::checkbox('is_completely_paid')}}
+			</div>
+		</div>
+
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<div class="row">
+					<div class="col-sm-3">
+						<div class="row">
+							<div class="col-sm-5"><b>Available</b></div>
+							<div class="col-sm-7"><b>Price</b></div>
+						</div>
+					</div>
+					<div class="col-sm-9">
+						<div class="row">
+							<div class="col-sm-2"><b>Paid Q</b></div>
+							<div class="col-sm-2"><b>Free Q</b></div>
+							<div class="col-sm-2"><b>GR Price</b></div>
+							<div class="col-sm-2"><b>GR Q</b></div>
+							<div class="col-sm-2"><b>CR Price</b></div>
+							<div class="col-sm-2"><b>CR Q</b></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		@foreach($items as $item)
+		<div class="form-group">
+			{{Form::label(null, $item->name, array('class' => 'col-sm-2 control-label'))}}
+			<div class="col-sm-10">
+				<div class="row">
+
+					<div class="col-sm-3">
+						<div class="row">
+							<div class="col-sm-5">
+								<p style="padding-top: 7px;">{{$stockDetails[$item->id]['good_quantity']}}</p>
+								{{Form::hidden('items['.$item->id.'][available_quantity]', $stockDetails[$item->id]['good_quantity'])}}
+							</div>
+							<div class="col-sm-7">
+								{{Form::input('number','items['.$item->id.'][price]',$item->current_selling_price, array('class' => 'form-control'),['step'=>0.01])}}
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-9">
+						<div class="row">
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][paid_quantity]',NULL, array('class' => 'form-control'))}}
+							</div>
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][free_quantity]',NULL, array('class' => 'form-control'))}}
+							</div>
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][good_return_price]',NULL, array('class' => 'form-control'),['step'=>0.01])}}
+							</div>
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][good_return_quantity]',NULL, array('class' => 'form-control'))}}
+							</div>
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][company_return_price]',NULL, array('class' => 'form-control'),['step'=>0.01])}}
+							</div>
+							<div class="col-sm-2">
+								{{Form::input('number','items['.$item->id.'][company_return_quantity]',NULL, array('class' => 'form-control'))}}
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+		@endforeach
+
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+			</div>
+		</div>
+
+		{{Form::close()}}
+
+	</div>
+</div>
+
 @stop
