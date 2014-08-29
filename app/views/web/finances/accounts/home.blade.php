3c3c8a18dcaac24712d377bcbe15db373b3b5fe1
Asanka Jagoda <asanka@thesanmark.com>
2014-08-29 15:13:21 +0530

    Create view finance UI.

--- app/views/web/finances/accounts/home.blade.php
+++ app/views/web/finances/accounts/home.blade.php
@@ -2,57 +2,78 @@
 
 @section('body')
 
-<h3>View Finance Accounts</h3>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">View Finance Accounts</h3>
+	</div>
+	<div class="panel-body">
 
-{{Form::open()}}
-<table border="1">
-	<tr>
-		<td>{{Form::label('name','name')}}</td>
-		<td>{{Form::text('name',$name)}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('bank_id','bank id')}}</td>
-		<td>{{Form::select('bank_id',$bankSelectBox,$bankId)}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('is_in_house','is in house')}}</td>
-		<td>{{Form::select('is_in_house',ViewButler::htmlSelectAnyYesNo (),$isInHouse)}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('is_active','is active')}}</td>
-		<td>{{Form::select('is_active',ViewButler::htmlSelectAnyYesNo (),$isActive)}}</td>
-	</tr>
-	<tr>
-		<td colspan="2">{{Form::submit('Submit')}}</td>
-	</tr>
-</table>
-{{Form::close()}}
+		{{Form::open(['class'=>'form-horizontal', 'role'=>'form'])}}
+		<br/>
+		<div class="form-group">
+			{{Form::label('name', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('name',$name, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('bank_id', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('bank_id',$bankSelectBox,$bankId, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('is_in_house', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('is_in_house',ViewButler::htmlSelectAnyYesNo (),$isInHouse, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('is_active', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('is_active',ViewButler::htmlSelectAnyYesNo (),$isActive, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-1 col-sm-3">
+				{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+			</div>
+		</div>
+		{{Form::close()}}
 
-<table border="1">
-	<tr>
-		<th>Name</th>
-		<th>Bank</th>
-		<th>Account Balance</th>
-		<th>Is In House</th>
-		<th>Is Active</th>
-		<th>Edit Account</th>
-	</tr>
-	@foreach($financeAccounts as $financeAccount)
-	<tr>
-		<th>{{$financeAccount->name}}</th>
-		
-		@if($financeAccount->bank_id==NULL)
-		<th></th>
-		@else
-		<th>{{$financeAccount->bank->name}}</th>
-		@endif
-		
-		<th>{{$financeAccount->account_balance}}</th>
-		<th>{{$financeAccount->is_in_house}}</th>
-		<th>{{$financeAccount->is_active}}</th>
-		<th>{{HTML::link ( URL::action ( 'finances.accounts.edit', [$financeAccount->id] ), 'Edit' ) }}</th>
-	</tr>
-	@endforeach
-</table>
+		<br/>
+
+		<table class="table table-striped" style="width: 80%;">
+			<tr>
+				<th>Name</th>
+				<th>Bank</th>
+				<th>Account Balance</th>
+				<th>Is In House</th>
+				<th>Is Active</th>
+				<th>Edit Account</th>
+			</tr>
+			<tbody>
+				@foreach($financeAccounts as $financeAccount)
+				<tr>
+					<td>{{$financeAccount->name}}</td>
+
+					@if($financeAccount->bank_id==NULL)
+					<td></td>
+					@else
+					<td>{{$financeAccount->bank->name}}</td>
+					@endif
+
+					<td>{{$financeAccount->account_balance}}</td>
+					<td>{{$financeAccount->is_in_house}}</td>
+					<td>{{$financeAccount->is_active}}</td>
+					<td>{{HTML::link ( URL::action ( 'finances.accounts.edit', [$financeAccount->id] ), 'Edit' ) }}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+	</div>
+</div>
+
 @stop
 
