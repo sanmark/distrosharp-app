ad25c68e6cd80c9c4b391e752247460e003d547d
Asanka Jagoda <asanka@thesanmark.com>
2014-08-29 13:43:53 +0530

    Create add finances UI.

--- app/views/web/finances/accounts/add.blade.php
+++ app/views/web/finances/accounts/add.blade.php
@@ -2,35 +2,52 @@
 
 @section('body')
 
-<h3>Add Finance Account</h3>
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Add Finance Account</h3>
+	</div>
+	<div class="panel-body">
 
-<ul>
-@foreach($errors->all() as $error)
-<li>{{$error}}</li>
-@endforeach
-</ul>
+		<ul>
+			@foreach($errors->all() as $error)
+			<li>{{$error}}</li>
+			@endforeach
+		</ul>
 
-{{Form::open()}}
-<table>
-	<tr>
-		<td>{{Form::label('name','name')}}</td>
-		<td>{{Form::text('name',null,['required'=>'required'])}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('bank_id','bank id')}}</td>
-		<td>{{Form::select('bank_id',$bankSelectBox)}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('is_in_house','is in house')}}</td>
-		<td>{{Form::checkbox('is_in_house')}}</td>
-	</tr>
-	<tr>
-		<td>{{Form::label('is_active','is active')}}</td>
-		<td>{{Form::checkbox('is_active')}}</td>
-	</tr>
-	<tr>
-		<td colspan="2">{{Form::submit('Submit')}}</td>
-	</tr>
-</table>
-{{Form::close()}}
+		{{Form::open(['class'=>'form-horizontal', 'role'=>'form'])}}
+		<br />
+		<div class="form-group">
+			{{Form::label('name', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::text('name',null, array('class' => 'form-control'),['required'=>'required'])}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('bank_id', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::select('bank_id',$bankSelectBox,null, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('is_in_house', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::checkbox('is_in_house', null, null, array('style' => 'margin-top: 10px;'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			{{Form::label('is_active', null, array('class' => 'col-sm-1 control-label'))}}
+			<div class="col-sm-3">
+				{{Form::checkbox('is_active', null, null, array('style' => 'margin-top: 10px;'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-1 col-sm-3">
+				{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+			</div>
+		</div>
+		{{Form::close()}}
+
+	</div>
+</div>
+
 @stop
