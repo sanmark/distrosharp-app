48d271b2833707fa0f2bda5b31893a4a4542c032
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-09-15 09:39:00 +0530

    SystemSettingButler::getValue() should return NULL if no result found.

--- app/butlers/SystemSettingButler.php
+++ app/butlers/SystemSettingButler.php
@@ -5,12 +5,18 @@ class SystemSettingButler
 
 	public static function getValue ( $systemSettingName )
 	{
+		$value = NULL ;
+
 		$requestObject	 = new \Models\SystemSettable() ;
 		$requestObject	 = $requestObject -> where ( 'name' , '=' , $systemSettingName ) ;
 
-		$systemSettable	 = $requestObject -> firstOrFail () ;
-		$systemSetting	 = $systemSettable -> systemSetting ;
-		$value			 = $systemSetting -> value ;
+		$systemSettable = $requestObject -> first () ;
+
+		if ( ! is_null ( $systemSettable ) )
+		{
+			$systemSetting	 = $systemSettable -> systemSetting ;
+			$value			 = $systemSetting -> value ;
+		}
 
 		return $value ;
 	}
