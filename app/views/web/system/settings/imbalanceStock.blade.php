c7376564acddb8997d01713118410e3f1353dc83
Asanka Jagoda <asanka@thesanmark.com>
2014-10-20 15:31:26 +0530

    #116 Create UI in inbalance stock.

--- app/views/web/system/settings/imbalanceStock.blade.php
+++ app/views/web/system/settings/imbalanceStock.blade.php
@@ -1,10 +1,22 @@
 @extends('web._templates.template')
 
 @section('body')
-<h2>Imbalance Stock</h2>
-{{Form::open()}}
-{{Form::label('imbalance_stock')}}
-{{Form::select('imbalance_stock', $stocksForHtmlSelect, $imbalanceStockId)}}
-{{Form::submit('Submit')}}
-{{Form::close()}}
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Imbalance Stock</h3>
+	</div>
+	<div class="panel-body">
+		{{Form::open(['class'=>'form-inline', 'role'=>'form'])}}
+		<div class="form-group inline-form">
+			{{Form::label('imbalance_stock', null, array('class' => 'control-label'))}}
+			{{Form::select('imbalance_stock', $stocksForHtmlSelect, $imbalanceStockId, array('class' => 'form-control'))}}
+		</div>
+		<div class="form-group inline-form">
+			{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+		</div>
+		{{Form::close()}}
+	</div>
+</div>
+
 @stop
