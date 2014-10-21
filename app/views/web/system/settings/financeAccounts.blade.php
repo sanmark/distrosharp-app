49c7ffad76e9be8b811a706a4e052688a12d0222
Asanka Jagoda <asanka@thesanmark.com>
2014-10-21 17:54:22 +0530

    #129 Create finance accounts UI.

--- app/views/web/system/settings/financeAccounts.blade.php
+++ app/views/web/system/settings/financeAccounts.blade.php
@@ -1,22 +1,32 @@
 @extends('web._templates.template')
 
 @section('body')
-<h2>Finance Accounts</h2>
-{{Form::open()}}
-<table>
-	<tr>
-		<td>Income Account</td>
-		<td>{{Form::select('income_account', $inHouseAccounts, $incomeAccount)}}</td>
-	</tr>
-	<tr>
-		<td>Expense Account</td>
-		<td>{{Form::select('expense_account', $inHouseAccounts, $expenseAccount)}}</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			{{Form::submit()}}
-		</td>
-	</tr>
-</table>
-{{Form::close()}}
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Finance Accounts</h3>
+	</div>
+	<div class="panel-body">
+		{{Form::open(['class'=>'form-horizontal', 'role'=>'form'])}}
+		<br />
+		<div class="form-group">
+			<div class="col-sm-2 control-label"><b>Income Account</b></div>
+			<div class="col-sm-3">
+				{{Form::select('income_account', $inHouseAccounts, $incomeAccount, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-2 control-label"><b>Expense Account</b></div>
+			<div class="col-sm-3">
+				{{Form::select('expense_account', $inHouseAccounts, $expenseAccount, array('class' => 'form-control'))}}
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-3 col-sm-offset-2">
+				{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+			</div>
+		</div>
+		{{Form::close()}}
+	</div>
+</div>
 @stop
