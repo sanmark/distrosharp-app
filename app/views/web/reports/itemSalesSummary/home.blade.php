d303bd5028b9108025b009f13a7d19cd5f1d4440
Asanka Jagoda <asanka@thesanmark.com>
2014-10-22 14:47:44 +0530

    #132 Create item sales summery report UI.

--- app/views/web/reports/itemSalesSummary/home.blade.php
+++ app/views/web/reports/itemSalesSummary/home.blade.php
@@ -1,46 +1,59 @@
 @extends('web._templates.template')
 
 @section('body')
-<h2>Item Sales Summary</h2>
-<table>
-	{{Form::open()}}
-	<tr>
-		<td>Rep</td>
-		<td>{{Form::select('rep_id', $repSelectBox, $repId)}}</td>
-	</tr>
-	<tr>
-		<td>From</td>
-		<td>{{Form::input('date', 'from_date', $fromDate)}}</td>
-	</tr>
-	<tr>
-		<td>To</td>
-		<td>{{Form::input('date', 'to_date', $toDate)}}</td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			{{Form::submit('Submit')}}
-		</td>
-	</tr>
-	{{Form::close()}}
-</table>
 
-@if(isset($items))
-<table>
-	<tr>
-		<th>Name</th>
-		<th>Total Free Amount</th>
-		<th>Total Paid Amount</th>
-	</tr>
-	@foreach($items as $item)
-	<tr>
-		<td>{{$item->name}}</td>
-		<td>{{$item->totalFreeAmount}}</td>
-		<td>{{$item->totalPaidAmount}}</td>
-	</tr>
-	@endforeach
-</table>
-@else
-Please define a criteria and press "Submit".
-@endif
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Item Sales Summary</h3>
+	</div>
+	<div class="panel-body">
+
+		<div class="panel panel-default">
+			<div class="panel-body">
+				{{Form::open(['class'=>'form-inline', 'role'=>'form'])}}
+				<div class="form-group inline-form">
+					{{Form::label('rep', null, array('class' => 'control-label'))}}
+					{{Form::select('rep_id', $repSelectBox, $repId, array('class' => 'form-control'))}}
+				</div>
+				<div class="form-group inline-form">
+					{{Form::label('from', null, array('class' => 'control-label'))}}
+					{{Form::input('date', 'from_date', $fromDate, array('class' => 'form-control'))}}
+				</div>
+				<div class="form-group inline-form">
+					{{Form::label('to', null, array('class' => 'control-label'))}}
+					{{Form::input('date', 'to_date', $toDate, array('class' => 'form-control'))}}
+				</div>
+				<div class="form-group inline-form">
+					{{Form::submit('Submit', array('class' => 'btn btn-default pull-right'))}}
+				</div>
+				{{Form::close()}}
+			</div>
+		</div>
+
+
+		@if(isset($items))
+		<table class="table table-striped" style="width: 55%;">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th class="text-right">Total Free Amount</th>
+					<th class="text-right">Total Paid Amount</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($items as $item)
+				<tr>
+					<td>{{$item->name}}</td>
+					<td class="text-right">{{$item->totalFreeAmount}}</td>
+					<td class="text-right">{{$item->totalPaidAmount}}</td>
+				</tr>
+				@endforeach
+			</tbody>
+		</table>
+		@else
+		<h4>Please define a criteria and press "Submit".</h4>
+		@endif
+	</div>
+</div>
 
 @stop
