d57335f8bfaa83f242b2d2506599f633fa4f2304
Asanka Jagoda <asanka@thesanmark.com>
2014-10-27 09:07:14 +0530

    #141 Create Select Rep. UI.

--- app/views/web/processes/sales/selectRep.blade.php
+++ app/views/web/processes/sales/selectRep.blade.php
@@ -1,10 +1,22 @@
 @extends('web._templates.template')
 
 @section('body')
-<h2>Select Rep</h2>
-{{Form::open()}}
-{{Form::label('rep_id', 'Rep')}}
-{{Form::select('rep_id', $repSelectBox, $currentRepId)}}
-{{Form::submit('Submit')}}
-{{Form::close()}}
+
+<div class="panel panel-default">
+	<div class="panel-heading">
+		<h3 class="panel-title">Select Rep</h3>
+	</div>
+	<div class="panel-body">
+		{{Form::open(['class'=>'form-inline', 'role'=>'form'])}}
+		<div class="form-group inline-form">
+			{{Form::label('rep_id', 'Rep', array('class' => 'control-label'))}}
+			{{Form::select('rep_id', $repSelectBox, $currentRepId, array('class' => 'form-control'))}}
+		</div>
+		<div class="form-group inline-form">
+			{{Form::submit('Submit', array('class' => 'btn btn-default'))}}
+		</div>
+		{{Form::close()}}
+	</div>
+</div>
+
 @stop
