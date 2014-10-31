89ca0e8573883176d389959ce16ab4a677be57d5
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-10-31 07:23:00 +0530

    #170 Change app title.

--- app/views/web/_templates/template.blade.php
+++ app/views/web/_templates/template.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>d-071-1-v2-app</title>
+		<title>Sanmark Distro#</title>
 
 		{{ HTML::style('packages/bootstrap/css/bootstrap.min.css') }}
 		{{ HTML::style('packages/bootstrap-material-design/css/ripples.min.css') }}
