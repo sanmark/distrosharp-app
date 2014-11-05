fe0ddab8e112b27697d6d4e7104bcf7b5f20794c
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-11-05 08:47:57 +0530

    #194 Disable activity logging in Super Admin mode.

--- app/butlers/ActivityLogButler.php
+++ app/butlers/ActivityLogButler.php
@@ -5,14 +5,17 @@ class ActivityLogButler
 
 	public static function add ( $message )
 	{
-		$activityLog = new Models\ActivityLog() ;
+		if ( ! SessionButler::isSuperAdminLoggedIn () )
+		{
+			$activityLog = new Models\ActivityLog() ;
 
-		$activityLog -> date_time	 = date ( "Y-m-d H:i:s" ) ;
-		$activityLog -> user_id		 = Auth::user () -> id ;
-		$activityLog -> message		 = $message ;
-		$activityLog -> url			 = Request::path () ;
+			$activityLog -> date_time	 = date ( "Y-m-d H:i:s" ) ;
+			$activityLog -> user_id		 = Auth::user () -> id ;
+			$activityLog -> message		 = $message ;
+			$activityLog -> url			 = Request::path () ;
 
-		return $activityLog -> save () ;
+			return $activityLog -> save () ;
+		}
 	}
 
 }
