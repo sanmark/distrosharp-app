53ee66157b805b603a018915f420686e00ba4516
Supun Budhajeewa <budhajeewa@thesanmark.com>
2014-12-30 07:50:25 +0530

    Install "a2way/laravel-hash".

--- app/config/app.php
+++ app/config/app.php
@@ -107,6 +107,7 @@ return array (
 		'Illuminate\View\ViewServiceProvider' ,
 		'Illuminate\Workbench\WorkbenchServiceProvider' ,
 		'A2way\LaravelTenantMigrate\LaravelTenantMigrateServiceProvider' ,
+		'A2Way\LaravelHash\LaravelHashServiceProvider'
 	) ,
 	/*
 	  |--------------------------------------------------------------------------
