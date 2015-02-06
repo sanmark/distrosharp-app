093eba3389b4f21ff5c10726e19686a9bc6250a0
Randika Srimal <email2randika@gmail.com>
2015-02-06 13:25:02 +0530

    #276 Add price,Total value of Quantity columns to stock confirm report

--- app/views/web/reports/stockConfirmReport/view.blade.php
+++ app/views/web/reports/stockConfirmReport/view.blade.php
@@ -7,18 +7,24 @@
 		<h3 class="panel-title">"<b>{{$stockDetails->stock->name}}</b>" Stock Confirmation Details on "<b>{{$stockDetails->date_time}}</b>"&nbsp;</h3>
 	</div>
 	<div class="panel-body">
-		<table class="table table-striped" style="width: 50%;">
+		<table class="table table-striped" style="width: 80%;">
 			<tr>
 				<th>Item</th>
+				<th class="text-right">Buying Price</th>
 				<th class="text-right">Good Item Quantity</th>
+				<th class="text-right">Total Value of Good Qnt</th>
 				<th class="text-right">Return Item Quantity</th>
+				<th class="text-right">Total Value of Return Qnt</th>
 			</tr>
 			<tbody>
 				@foreach($stockConfirmationDetails as $stockConfirmationDetail)
 				<tr>
 					<td>{{$stockConfirmationDetail->item->name}}</td>
+					<td class="text-right">{{$stockConfirmationDetail->item->current_buying_price}}</td>
 					<td class="text-right">{{$stockConfirmationDetail->good_item_quantity}}</td>
+					<td class="text-right">{{$stockConfirmationDetail->item->current_buying_price*$stockConfirmationDetail->good_item_quantity}}</td>
 					<td class="text-right">{{$stockConfirmationDetail->return_item_quantity}}</td>
+					<td class="text-right">{{$stockConfirmationDetail->item->current_buying_price*$stockConfirmationDetail->return_item_quantity}}</td>
 				</tr>
 				@endforeach
 			</tbody>
